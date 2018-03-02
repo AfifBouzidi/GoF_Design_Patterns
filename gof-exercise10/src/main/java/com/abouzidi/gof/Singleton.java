9d428684fab43270afe8a630cd521fdd3e97f62d
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-03-02 02:25:38 +0100

    Singleton: Inner static helper class

---
+++ gof-exercise10/src/main/java/com/abouzidi/gof/Singleton.java
@@ -0,0 +1,16 @@
+package com.abouzidi.gof;
+
+public class Singleton {
+
+	private Singleton() {
+
+	}
+
+	private static class SingletonHelper {
+		private static final Singleton instance = new Singleton();
+	}
+
+	public static Singleton getInstance() {
+		return SingletonHelper.instance;
+	}
+}
